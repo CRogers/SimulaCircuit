d603d956e042eac40fdc48d554daf94fd130a1d0
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-01 23:03:14 +0100

    Created flip flop

---
+++ SimulaCircuit/StateHolding/FlipFlop.cs
@@ -0,0 +1,21 @@
+﻿using SimulaCircuit.SignalGenerators;
+
+namespace SimulaCircuit.StateHolding
+{
+    public class FlipFlop : IOutput
+    {
+        public Clock Clock { get; set; }
+        public IOutput Input { get; set; }
+
+        public bool Output { get; private set; }
+
+        public FlipFlop(Clock clock, IOutput input)
+        {
+            Input = input;
+            Output = input.Output;
+
+            Clock = clock;
+            clock.Tick += () => Output = Input.Output;
+        }
+    }
+}
