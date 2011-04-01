6c5aa05050c858e3ee8e7ab3cebe87a58c17d06f
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-01 23:55:35 +0100

    Added/improved tests

--- Tests/StateHolding.cs
+++ Tests/StateHolding.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulaCircuit;
 using SimulaCircuit.Gates;
 using SimulaCircuit.SignalGenerators;
 using SimulaCircuit.StateHolding;
@@ -9,32 +12,33 @@ namespace Tests
     [TestClass]
     public class StateHolding
     {
-        Clock c = new Clock(10);
+        Clock c = new Clock(1);
         Wire t = new Wire(new True());
         Wire f = new Wire(new False());
+        TimeSpan ts = new TimeSpan(1000);
 
         bool ready = false;
 
-        private void SetReady()
+
+        public StateHolding()
         {
-            ready = true;
+            c.AfterAll += () => ready = true;
         }
+
 
         private void WaitUntilReady()
         {
             while(!ready)
-                Thread.Sleep(1);
+            {
+            }
             ready = false;
         }
 
         [TestMethod]
         public void FlipFlop()
         {
-
             FlipFlop ff = new FlipFlop(c, t);
             Assert.IsTrue(ff.Output);
-
-            c.Tick += SetReady;
 
             ff.Input = f;
             WaitUntilReady();
@@ -46,8 +50,42 @@ namespace Tests
             ff.Input = t;
             WaitUntilReady();
             Assert.IsTrue(ff.Output);
+        }
 
-            c.Tick -= SetReady;
+        [TestMethod]
+        public void ShiftRegister()
+        {
+            var ffs = new FlipFlop(c, f).Unfold(ff => new FlipFlop(c, ff), max: 8).ToArray();
+            ffs[0].Input = t;
+
+            for (int i = 0; i < 8; i++)
+            {
+                WaitUntilReady();
+                for (int j = 0; j < i; j++)
+                    Assert.IsTrue(ffs[j].Output);
+            }
+        }
+
+        [TestMethod]
+        public void LoopDevice()
+        {
+            var ffs = new FlipFlop(c, f).Unfold(ff => new FlipFlop(c, ff), max: 8).ToArray();
+            ffs[0].Input = new Inverter(ffs[ffs.Length - 1]);
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                WaitUntilReady();
+                for (int j = 0; j < i; j++)
+                    Assert.IsTrue(ffs[j].Output);
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                WaitUntilReady();
+                for (int j = 0; j < i; j++)
+                    Assert.IsFalse(ffs[j].Output);
+            }
         }
     }
 }
