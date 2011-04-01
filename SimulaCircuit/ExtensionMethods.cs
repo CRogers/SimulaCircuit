9a833ab1a3ee0b7d9f75a5f035b9e458f37e8570
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-01 23:53:15 +0100

    Added Unfold Extension method

---
+++ SimulaCircuit/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimulaCircuit
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<T> Unfold<T>(this T seed, Func<T,T> func, Predicate<T> endCondition = null, int max = int.MaxValue)
+        {
+            if (endCondition == null)
+                endCondition = x => false;
+
+            var t = seed;
+            for (int i = 0; !endCondition(t) && i < max; i++)
+                yield return t = func(t);
+        }
+    }
+}
