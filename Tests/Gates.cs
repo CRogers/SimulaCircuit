d826f558c1f78b781b728e996de1bd857e99316b
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-01 23:03:26 +0100

    Made tests for flip flop

--- Tests/Gates.cs
+++ Tests/Gates.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulaCircuit;
 using SimulaCircuit.Gates;
 
 namespace Tests
---
+++ Tests/StateHolding.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimulaCircuit.Gates;
+using SimulaCircuit.SignalGenerators;
+using SimulaCircuit.StateHolding;
+
+namespace Tests
+{
+    [TestClass]
+    public class StateHolding
+    {
+        Clock c = new Clock(10);
+        Wire t = new Wire(new True());
+        Wire f = new Wire(new False());
+
+        bool ready = false;
+
+        private void SetReady()
+        {
+            ready = true;
+        }
+
+        private void WaitUntilReady()
+        {
+            while(!ready)
+                Thread.Sleep(1);
+            ready = false;
+        }
+
+        [TestMethod]
+        public void FlipFlop()
+        {
+
+            FlipFlop ff = new FlipFlop(c, t);
+            Assert.IsTrue(ff.Output);
+
+            c.Tick += SetReady;
+
+            ff.Input = f;
+            WaitUntilReady();
+            Assert.IsFalse(ff.Output);
+
+            WaitUntilReady();
+            Assert.IsFalse(ff.Output);
+
+            ff.Input = t;
+            WaitUntilReady();
+            Assert.IsTrue(ff.Output);
+
+            c.Tick -= SetReady;
+        }
+    }
+}
