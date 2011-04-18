9f1b239b11a11db46eec8b8aa4ad6e9195cd4e61
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-18 01:56:06 +0100

    Left and Right now change on canvas when changed in object

--- SimulaCircuit.GUI/Data/CircuitItem.cs
+++ SimulaCircuit.GUI/Data/CircuitItem.cs
@@ -1,26 +1,55 @@
 ﻿using System;
+using System.Windows.Controls;
 using System.Xml.Serialization;
+using WPF.JoshSmith.Controls;
 
 namespace SimulaCircuit.GUI.Data
 {
     [Serializable]
     public class CircuitItem<T> : XmlSerializable<CircuitItem<T>>, IOutput where T : IOutput
     {
+        private int left;
         [XmlAttribute]
-        public int Left { get; set; }
+        public int Left
+        {
+            get { return left; }
+            set
+            {
+                left = value;
+                DragCanvas.SetLeft(Canvas, value);
+            }
+        }
 
+        private int top;
         [XmlAttribute]
-        public int Top { get; set; }
+        public int Top
+        {
+            get { return top; }
+            set
+            {
+                top = value;
+                DragCanvas.SetTop(Canvas, value);
+            }
+        }
 
         [XmlAttribute]
         public string ItemType { get; set; }
 
+        public Canvas Canvas;
         private T item;
 
 
         public bool Output
         {
             get { return item.Output; }
+        }
+
+
+        public CircuitItem(DragCanvas dc, T item)
+        {
+            this.item = item;
+            Canvas = new Canvas();
+            dc.Children.Add(Canvas);
         }
     }
 }
